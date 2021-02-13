e8580b913dd392ee5d656b8f4bc3b601b74a94e9
wesfolz <wesleyfolz@gmail.com>
2021-02-12 22:46:12 -0500

    Updates

--- precache-manifest.76b2042affc02e37d91d8e585563db54.js
+++ precache-manifest.76b2042affc02e37d91d8e585563db54.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/static/media/gradient.27bd57cb.png"
   },
   {
-    "revision": "6264a40573c2f6440993",
+    "revision": "e8dddf205fcf4fa1706c",
     "url": "/static/css/main.83c08f24.chunk.css"
   },
   {
@@ -200,8 +200,8 @@ self.__precacheManifest = [
     "url": "/static/media/xilinx.fb652692.svg"
   },
   {
-    "revision": "6264a40573c2f6440993",
-    "url": "/static/js/main.31e20b40.chunk.js"
+    "revision": "e8dddf205fcf4fa1706c",
+    "url": "/static/js/main.84f1ab3c.chunk.js"
   },
   {
     "revision": "be02bfeeba9501ca2d58d13417131563",
@@ -292,12 +292,12 @@ self.__precacheManifest = [
     "url": "/static/media/source-code-pro-latin-300.dccbac56.woff2"
   },
   {
-    "revision": "ffa5c77d0ba471964e2edf117c5e52cf",
-    "url": "/static/media/source-code-pro-latin-200.ffa5c77d.woff2"
-  },
-  {
     "revision": "5150e99c19321c28973e47f9569d2777",
     "url": "/static/media/source-code-pro-latin-200italic.5150e99c.woff2"
+  },
+  {
+    "revision": "ffa5c77d0ba471964e2edf117c5e52cf",
+    "url": "/static/media/source-code-pro-latin-200.ffa5c77d.woff2"
   },
   {
     "revision": "9e3292f8b91cbb18756a46d39b802e48",
@@ -320,44 +320,44 @@ self.__precacheManifest = [
     "url": "/static/media/source-code-pro-latin-500italic.04d782d8.woff2"
   },
   {
-    "revision": "3036a9fd64c64be915eb1dbcfb6a51cf",
-    "url": "/static/media/source-code-pro-latin-600.3036a9fd.woff2"
-  },
-  {
     "revision": "b4c3891c92598de51f1470a6302a2f67",
     "url": "/static/media/source-code-pro-latin-600italic.b4c3891c.woff2"
   },
   {
-    "revision": "fc9b51aa041ab01766a7d13cca7e230c",
-    "url": "/static/media/source-code-pro-latin-700italic.fc9b51aa.woff2"
-  },
-  {
-    "revision": "e13ecdc77b68244d5e0d1149a35726e1",
-    "url": "/static/media/source-code-pro-latin-900.e13ecdc7.woff2"
+    "revision": "3036a9fd64c64be915eb1dbcfb6a51cf",
+    "url": "/static/media/source-code-pro-latin-600.3036a9fd.woff2"
   },
   {
     "revision": "f85c431d467f00f1f44b1a2dc3792852",
     "url": "/static/media/source-code-pro-latin-700.f85c431d.woff2"
   },
   {
+    "revision": "e13ecdc77b68244d5e0d1149a35726e1",
+    "url": "/static/media/source-code-pro-latin-900.e13ecdc7.woff2"
+  },
+  {
+    "revision": "fc9b51aa041ab01766a7d13cca7e230c",
+    "url": "/static/media/source-code-pro-latin-700italic.fc9b51aa.woff2"
+  },
+  {
     "revision": "9e39d561554343bf27d2acf561bf256e",
     "url": "/static/media/source-code-pro-latin-900italic.9e39d561.woff2"
-  },
-  {
-    "revision": "62a30baa98bb767d790e8780b73e2b1c",
-    "url": "/static/media/source-code-pro-latin-200italic.62a30baa.woff"
-  },
-  {
-    "revision": "7082c98886a824910e49c07a1025f114",
-    "url": "/static/media/source-code-pro-latin-300italic.7082c988.woff"
   },
   {
     "revision": "558e932a85350db3380c8daa75c396ab",
     "url": "/static/media/source-code-pro-latin-200.558e932a.woff"
   },
   {
+    "revision": "7082c98886a824910e49c07a1025f114",
+    "url": "/static/media/source-code-pro-latin-300italic.7082c988.woff"
+  },
+  {
     "revision": "de712560ce6208f9fb93e71af1dd0f8b",
     "url": "/static/media/source-code-pro-latin-300.de712560.woff"
+  },
+  {
+    "revision": "62a30baa98bb767d790e8780b73e2b1c",
+    "url": "/static/media/source-code-pro-latin-200italic.62a30baa.woff"
   },
   {
     "revision": "17ea4760cdf09ade45a20bce5f2933a9",
@@ -368,12 +368,12 @@ self.__precacheManifest = [
     "url": "/static/media/source-code-pro-latin-400italic.18dd4f87.woff"
   },
   {
-    "revision": "cfc748ca1b98f6855c39ccff0f6414b8",
-    "url": "/static/media/source-code-pro-latin-500italic.cfc748ca.woff"
-  },
-  {
     "revision": "3f58f0647ec74049ee3ba6cef1a5fde0",
     "url": "/static/media/source-code-pro-latin-500.3f58f064.woff"
+  },
+  {
+    "revision": "cfc748ca1b98f6855c39ccff0f6414b8",
+    "url": "/static/media/source-code-pro-latin-500italic.cfc748ca.woff"
   },
   {
     "revision": "fead30eb2d488adb1b3ddf744aeb7995",
@@ -384,6 +384,10 @@ self.__precacheManifest = [
     "url": "/static/media/source-code-pro-latin-600.5a085db2.woff"
   },
   {
+    "revision": "e690a100108349e491a9803509e8f61e",
+    "url": "/static/media/source-code-pro-latin-700italic.e690a100.woff"
+  },
+  {
     "revision": "b837c15c474e3654dabab85ad647cde6",
     "url": "/static/media/source-code-pro-latin-700.b837c15c.woff"
   },
@@ -392,15 +396,11 @@ self.__precacheManifest = [
     "url": "/static/media/source-code-pro-latin-900.c48732b5.woff"
   },
   {
-    "revision": "e690a100108349e491a9803509e8f61e",
-    "url": "/static/media/source-code-pro-latin-700italic.e690a100.woff"
-  },
-  {
     "revision": "9d8f72df730a2fc2d709",
     "url": "/static/css/2.35af7af6.chunk.css"
   },
   {
-    "revision": "2468a909cb633d47751cdc93df0440df",
+    "revision": "84b1ef33ab52a3d979bc13b5644b4165",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c88d943af658399d5e9caef8669aadd9.js"
+  "/precache-manifest.76b2042affc02e37d91d8e585563db54.js"
 );
 
 workbox.clientsClaim();
